39ae8c55bd257148ee839d2be8f6979d1beb4ce2
atabek444 <toktomambetov444@gmail.com>
2023-02-26 20:42:41 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,203 @@
+const colorNames = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+];
+const board = document.querySelector("#board")
+
+// setAttribute
+// getAttribute
+for (let i = 0; i < 208; i++) {
+    const square = document.createElement("div")
+    board.append(square)
+    square.setAttribute("class", "square")
+    square.addEventListener("mouseover", () => {
+        setColor(square)
+    })
+    square.addEventListener("mouseout", () => {
+        removeColor(square)
+    })
+}
+
+function setColor(box) {
+    const color = randomColor()
+    box.style.background = color
+    box.style.transform = "scale(1.2)"
+    box.style.boxShadow = `2px  2px 4px ${color}, 0 0 10px ${color}`
+
+
+}
+function removeColor(box){
+    box.style.background = "#424242"
+    box.style.transform = "scale(1)"
+    box.style.boxShadow = `0 0 2px #000`
+}
+function randomColor(){
+    const index = Math.round(Math.random() * colorNames.length)
+    return colorNames[index]
+}
+
+let btn = document.querySelectorAll("button")
+btn[0].style.background = "DarkBlue"
+btn[0].style.padding = "10px 25px"
+btn[0].style.borderRadius = "5px"
+btn[0].style.border = "none"
+btn[0].style.margin = "40px 20px 0 20px"
+btn[0].style.color = "white"
+
+btn[1].style.background = "DarkRed"
+btn[1].style.padding = "10px 25px"
+btn[1].style.borderRadius = "5px"
+btn[1].style.border = "none"
+btn[1].style.margin = "40px 20px 0 20px"
+btn[1].style.color = "white"
+const container = document.querySelector(".container div")
+console.log(container)
+btn[0].addEventListener("click",() =>{
+
+})
